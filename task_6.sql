91ac948cad943e5b82a8eb7d8b1432f0fe2f12f5
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-08-11 13:19:46 +0300

    Add sql script for multiple data insertion

---
+++ task_6.sql
@@ -0,0 +1,6 @@
+-- A sql script for multiple data insertion into the table customers in the alx_book_store database
+USE alx_book_store;
+INSERT INTO customer (customer_id, customer_name, email, address) 
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com',124 Happiness Ave.),
+VALUES (3,'Obed Ehoneah', 'eobed@sandtech.com',125 Happiness Ave.),
+VALUES (4,'Nehemial Kamolu', 'nkamolu@sandtech.com',126 Happiness Ave.)

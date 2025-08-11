af89a0f87a1e202385a5bc71b9d0e741fcf02395
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-08-11 11:59:22 +0300

    Add a database connection & crate a database script

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MySQLServer.py
+This script connects to a MySQL server and creates a database if it does not exist.
+"""
+
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_to_create = "alx_book_store"
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+    )
+    cursor = conn.cursor()
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS { db_to_create }")
+    print(f"Database '{db_to_create}' created successfully.")
+except mysql.connector.Error as e:
+    print(f"Missing environment variable: {e}")
+
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'conn' in locals() and conn.is_connected():
+        conn.close()
+        print("Connection closed.")

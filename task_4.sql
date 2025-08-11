329b74d7e43356f4aa61f5b86ae67c05840c3dc7
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-08-11 13:00:16 +0300

    Add a sql script to print the descrbition of a specific table in the db

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,13 @@
 -- A sql script print the description of the tables in the alx_book_store database
-use alx_book_store;
-DESCRIBE Books;
+
+USE alx_book_store;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';

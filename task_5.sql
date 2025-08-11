0517487fd4482fb0b557f39c732668e70272823a
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-08-11 13:08:32 +0300

    Add sql script fro data insertion

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 -- A sql script for data insertion into the table customers in the alx_book_store database
 USE alx_book_store;
-INSERT INTO Customer (customer_id, customer_name, email, address) 
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com','123 Happiness Ave');
+INSERT INTO customer (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com',123 Happiness Ave.);

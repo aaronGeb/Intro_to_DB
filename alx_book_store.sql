dd5d31ce9d4fa28f24d7aa2100cfdc038600a7a0
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-08-11 13:29:00 +0300

    Add sql script to creat a book store schema

---
+++ alx_book_store.sql
@@ -0,0 +1,45 @@
+-- Creates a MySQL database named `alx_book_store` and its associated tables.
+-- This script should be run in a MySQL environment.
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+  
+-- CREATES A TABLE `books` IN THE `alx_book_store` DATABASE
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+-- CREATES A TABLE `authors` IN THE `alx_book_store` DATABASE
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) 
+);
+
+-- CREATES A TABLE `customers` IN THE `alx_book_store` DATABASE
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT
+);
+-- CREATES A TABLE `orders` IN THE `alx_book_store` DATABASE
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+-- CREATES A TABLE `order_items` IN THE `alx_book_store` DATABASE
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+
